f5e4c16a3c15dcbe2e36db15234d3c7e38cc290c
Daryl Shannon <32194715+dshannon-ifs@users.noreply.github.com>
2020-11-16 11:08:53 +0000

    Bug fix to _getPageRotation

--- tcpdi_parser.php
+++ tcpdi_parser.php
@@ -1423,7 +1423,7 @@ class tcpdi_parser {
                 return false;
             } else {
                 $res = $this->_getPageRotation($obj[1][1]['/Parent']);
-                if ($res[0] == PDF_TYPE_OBJECT)
+                if ($res && $res[0] == PDF_TYPE_OBJECT)
                     return $res[1];
                 return $res;
             }
